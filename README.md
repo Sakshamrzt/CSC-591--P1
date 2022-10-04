053b5fff0993728fc4924d28fb4aa8bfd6d43105
Saksham <sthakur5@ncsu.edu>
2022-10-04 00:43:54 -0400

    Added files and data.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Unit Project 1: DNN Pruning via NNI
+----
+## Table Of Contents
+- [Description](#description)
+- [Team](#team)
+----
+## Description
+Main scripts lies under scr directory.
+File  | Usage
+------------- | -------------
+pruner_vae.py  | Main file which prunes the model and peforms training/evaluation.
+vae1.py | File containing vae mode.
+----
+
+## Team
+Name  | Unity id
+------------- | -------------
+Saksham Thakur  | sthakur5
+Xinning Hui | xhui
+---
+
---
+++ src/pruner_vae.py
@@ -0,0 +1,112 @@
+"""
+Pruning Quickstart
+==================
+Here is a three-minute video to get you started with model pruning.
+..  youtube:: wKh51Jnr0a8
+    :align: center
+Model pruning is a technique to reduce the model size and computation by reducing model weight size or intermediate state size.
+There are three common practices for pruning a DNN model:
+#. Pre-training a model -> Pruning the model -> Fine-tuning the pruned model
+#. Pruning a model during training (i.e., pruning aware training) -> Fine-tuning the pruned model
+#. Pruning a model -> Training the pruned model from scratch
+NNI supports all of the above pruning practices by working on the key pruning stage.
+Following this tutorial for a quick look at how to use NNI to prune a model in a common practice.
+"""
+
+# %%
+# Preparation
+# -----------
+#
+# In this tutorial, we use a simple model and pre-trained on MNIST dataset.
+# If you are familiar with defining a model and training in pytorch, you can skip directly to `Pruning Model`_.
+
+import torch
+import time
+from torch import nn, optim
+from nni.compression.pytorch.speedup import ModelSpeedup
+from vae1 import VAE, loss_function, trainer, evaluator, device
+from nni.compression.pytorch.pruning import L2NormPruner, L1NormPruner,FPGMPruner
+
+# # define the model
+
+model = VAE().to(device)
+
+# show the model structure, note that pruner will wrap the model layer.
+print(model)
+
+# %%
+
+# define the optimizer and criterion for pre-training
+time_sum=0
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+epochs=1
+time_sum1=0
+# pre-train and evaluate the model on MNIST dataset
+for epoch in range(epochs):
+    t1=time.time()
+    trainer(model,epoch,optimizer)
+    t2=time.time()
+    evaluator(epoch,model)
+    time_sum+=t2-t1
+    time_sum1+=time.time()-t2
+
+print("Average training time of unpruned model is ",time_sum/epochs)
+print("Average evaluation time of unpruned model is ",time_sum1/epochs)
+
+torch.save(model.state_dict(), "model1.pt")
+
+# %%
+# Pruning Model
+# -------------
+#
+# Using L1NormPruner to prune the model and generate the masks.
+# Usually, a pruner requires original model and ``config_list`` as its inputs.
+# Detailed about how to write ``config_list`` please refer :doc:`compression config specification <../compression/compression_config_list>`.
+#
+# The following `config_list` means all layers whose type is `Linear` or `Conv2d` will be pruned,
+# except the layer named `fc3`, because `fc3` is `exclude`.
+# The final sparsity ratio for each layer is 50%. The layer named `fc3` will not be pruned.
+
+config_list =  [{ 'sparsity': 0.5, 'op_types': ['Linear']},{'exclude': True, 'op_names': ['fc4']}]
+# config_list =  [{ 'sparsity': 0.2, 'op_types': ['Linear']},{'exclude': True, 'op_names': ['fc4']}]
+# config_list =  [{ 'sparsity': 0.8, 'op_types': ['Linear']},{'exclude': True, 'op_names': ['fc4']}]
+
+
+pruner = FPGMPruner(model, config_list)
+# pruner = L1NormPruner(model, config_list)
+
+
+# # compress the model and generate the masks
+_, masks = pruner.compress()
+pruner.show_pruned_weights()
+# show the masks sparsity
+for name, mask in masks.items():
+    print(name, ' sparsity : ', '{:.2}'.format(mask['weight'].sum() / mask['weight'].numel()))
+
+
+pruner._unwrap_model()
+
+# # speedup the model, for more information about speedup, please refer :doc:`pruning_speedup`.
+
+
+ModelSpeedup(model, torch.rand(128, 1, 28, 28).to(device), masks).speedup_model()
+
+
+# optimizer = SGD(model.parameters(), 1e-2)
+torch.save(model.state_dict(), "model2.pt")
+print(model,"is model")
+
+time_sum=0
+time_sum1=0
+
+for epoch in range(1):
+    t1=time.time()
+    trainer(model,epoch,optimizer)
+    t2=time.time()
+    evaluator(epoch,model)
+    time_sum+=t2-t1
+    time_sum1+=time.time()-t2
+    
+
+print("Average training time of pruned model is ",time_sum/epochs)
+print("Average evaluation time of pruned model is ",time_sum1/epochs)
